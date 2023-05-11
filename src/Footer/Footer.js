19cc42c86659eaa6e4376d52d4c2f6354f4a6a62
ShubhangiG23 <shubhajinkyag2611@gmail.com>
2023-05-11 17:06:01 +0530

    first commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,40 @@
+import "../Footer/Footer.css";
+
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
+
+function Footer() {
+  return( 
+  <div >
+    
+    <br/><br/><br/><br/><br/>
+
+  <footer className="footer">
+            <div className="footer-container">
+                <div className="item1">
+                    
+                </div>
+
+                <div className="item2">
+                    <span >Copyright All Rights Are Reserved Since 2023-2024. </span>
+                   
+                  
+                     
+                        
+
+                  
+                </div>
+              
+               
+               
+
+               
+            </div>
+        </footer>
+        </div>
+        );
+}
+
+export default Footer;
